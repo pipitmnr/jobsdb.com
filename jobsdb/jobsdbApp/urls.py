f9bface5ca42d9fe003d5e24a50aec71f79f0131
pipitmnr <syafitri@alterra.id>
2019-12-12 22:43:30 +0700

    hasil search detail lowongan

--- jobsdb/jobsdbApp/urls.py
+++ jobsdb/jobsdbApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.beranda, name='beranda'),
+    path('search-result/', views.searchResult, name='search-result'),
+    path('detail-lowongan/', views.detailLowongan, name='detail-lowongan'),
 ]
--- jobsdb/jobsdbApp/views.py
+++ jobsdb/jobsdbApp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import redirect
 # Create your views here.
 def beranda(request):
     return render(request, 'jobsdbApp/beranda.html')
+def searchResult(request):
+    return render(request, 'jobsdbApp/search-result.html')
+def detailLowongan(request):
+    return render(request, 'jobsdbApp/detail-lowongan.html')
