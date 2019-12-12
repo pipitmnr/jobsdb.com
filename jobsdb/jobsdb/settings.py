ef4f110fb50a00ef268f8df37daee6ca7efc3507
pipitmnr <syafitri@alterra.id>
2019-12-12 18:17:30 +0700

    header footer

--- jobsdb/jobsdb/settings.py
+++ jobsdb/jobsdb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobsdbApp.apps.JobsdbappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
